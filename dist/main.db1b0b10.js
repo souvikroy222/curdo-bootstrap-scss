b11d9a7e655bc48fb3e573137c859f95bdd1bca1
souvikroy222 <69359546+souvikroy222@users.noreply.github.com>
2022-05-31 22:08:31 +0530

    padding and margin fix

--- dist/main.db1b0b10.js
+++ dist/main.db1b0b10.js
@@ -166,7 +166,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "54688" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "62281" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
